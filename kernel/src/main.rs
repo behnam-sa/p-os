11d7f10fbd7edff7ad973d08fdb4374a763b2a2f
Behnam Sa <saedi.behnam.336@gmail.com>
2023-10-13 18:56:36 +0330

    Add logger

--- kernel/src/main.rs
+++ kernel/src/main.rs
@@ -1,23 +1,28 @@
 #![no_std]
 #![no_main]
 
+mod logger;
+
+use crate::logger::init_logger;
+use bootloader_api::BootInfo;
 use core::panic::PanicInfo;
+
+bootloader_api::entry_point!(kernel_main);
 
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
     loop {}
 }
 
-use bootloader_api::BootInfo;
-
-bootloader_api::entry_point!(kernel_main);
-
 fn kernel_main(boot_info: &'static mut BootInfo) -> ! {
-    if let Some(framebuffer) = boot_info.framebuffer.as_mut() {
-        for byte in framebuffer.buffer_mut() {
-            *byte = 0x90;
-        }
-    }
-    
+    let framebuffer = boot_info.framebuffer.as_mut().unwrap();
+
+    let framebuffer_info = framebuffer.info().clone();
+    let raw_frame_buffer = framebuffer.buffer_mut();
+
+    init_logger(raw_frame_buffer, framebuffer_info);
+
+    log::info!("Hello from kernel!");
+
     loop {}
 }
